e0487eff792e7fded18ce1c3782ddb3c488df3b6
unknown <hrodriguez-2023278@kinal.edu.gt>
2025-02-11 22:52:00 -0600

    Se crea la estructura basica del proyecto

---
+++ configs/documentacion.js
@@ -0,0 +1,29 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Supermarket API",
+            version:"1.0.0",
+            description: "API para sistema de supermercado",
+            contact:{
+                name: "Harol Rodriguez",
+                email: "hrodriguez-2023278@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/supermarket/v1"
+            }
+        ]
+    },
+    apis:[
+        //"./src/auth/*.js",
+        
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+export { swaggerDocs, swaggerUI }
